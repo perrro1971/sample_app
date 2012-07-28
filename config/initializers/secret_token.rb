012f349aaa80ba42ec51823dd5bceb0ac1e89219
Walter Semelshtein <walter@pictomotion.com>
2012-07-28 14:20:42 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f37e28a75c0076e89d856aff42e4b10aea9d2bece91210abf1c5e367f5d0f16b411aae3cecad78f24377b9c9ca7636de25142210f3ddaa1c0491b4fb805ba29d'
