ad0f78417c241b8e5cff7fe604b9bf3a69a8256b
Walter Semelshtein <walter@pictomotion.com>
2012-08-03 18:03:18 -0300

    sign up mail confimation case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -81,6 +81,15 @@ describe User do
     it { should be_valid}
   end
   
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMpLe.CoM"  }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end  
+  end
+  
   describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user){ User.find_by_email(@user.email) }
